47da1055a8b391ac2a102910a47811bdf41ac216
Marlon Prudente <marlonmateuspr@gmail.com>
2016-10-13 11:52:59 -0300

    Construcao do Banco2

--- bancoreg16bits.vhd
+++ bancoreg16bits.vhd
@@ -21,20 +21,35 @@ architecture a_bancoreg16bits of bancoreg16bits is
 		port(	clock		:	in std_logic;
 				reset		:	in std_logic;
 				wr_enable	:	in std_logic;
+				op			:	in unsigned(1 downto 0);
 				data_in		:	in unsigned(15 downto 0);
 				data_out	:	out unsigned (15 downto 0)
+	);
+	end component;
+
+	component mux16bits is
+		port(	entr0	:	in unsigned(15 downto 0);
+				entr1	:	in unsigned(15 downto 0);
+				entr2	:	in unsigned(15 downto 0);
+				entr3	:	in unsigned(15 downto 0);
+				entr4	:	in unsigned(15 downto 0);
+				entr5	:	in unsigned(15 downto 0);
+				entr6	:	in unsigned(15 downto 0);
+				entr7	:	in unsigned(15 downto 0);
+				op		: 	in unsigned(2 downto 0);
+				saida	:	out unsigned(15 downto 0)
 	);
 	end component;
 
 signal clk,rst : std_logic;
 
 begin 
-	reg0: reg16bits	port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=>"0000000000000000");
-	reg1: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg2: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg3: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg4: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg5: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg6: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
-	reg7: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> ,data_out=> );
+	reg0: reg16bits	port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> "0",data_out=>"0000000000000000");
+	reg1: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr1);
+	reg2: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr2);
+	reg3: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr3);
+	reg4: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr4);
+	reg5: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr5);
+	reg6: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr6);
+	reg7: reg16bits port map(clock=>clk,reset=>rst,wr_enable=>wr_en,data_in=> data_in,data_out=> entr7);
 end architecture;
--- mux16bits.vhd
+++ mux16bits.vhd
@@ -5,13 +5,27 @@ use ieee.numeric_std.all;
 entity mux16bits is
 	port(	entr0	:	in unsigned(15 downto 0);
 			entr1	:	in unsigned(15 downto 0);
-			op		: 	in std_logic;
+			entr2	:	in unsigned(15 downto 0);
+			entr3	:	in unsigned(15 downto 0);
+			entr4	:	in unsigned(15 downto 0);
+			entr5	:	in unsigned(15 downto 0);
+			entr6	:	in unsigned(15 downto 0);
+			entr7	:	in unsigned(15 downto 0);
+			op		: 	in unsigned(2 downto 0);
 			saida	:	out unsigned(15 downto 0)
 
 	);
+end entity;
 
 architecture a_mux16bits of mux16bits is
-	saida <=	entr0 when op="0" else
-				entr1 when op="1" else
+	saida <=	entr0 when op="000" else
+				entr1 when op="001" else
+				entr2 when op="010" else
+				entr3 when op="011" else
+				entr4 when op="100" else
+				entr5 when op="101" else
+				entr6 when op="110" else
+				entr7 when op="111" else
 				"0000000000000000";
+
 end architecture;
