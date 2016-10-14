1ef8ef0596c65b67a3b6af0797aa3c5bde12f95e
Marlon Prudente <marlonmateuspr@gmail.com>
2016-10-14 00:00:21 -0300

    Ultimo commit

--- mux16bits_tb.vhd
+++ mux16bits_tb.vhd
@@ -10,57 +10,46 @@ architecture a_mux16bits_tb of mux16bits_tb is
 		port(
 			entr0	:	in unsigned(15 downto 0);
 			entr1	:	in unsigned(15 downto 0);
-			entr2	:	in unsigned(15 downto 0);
-			entr3	:	in unsigned(15 downto 0);
-			entr4	:	in unsigned(15 downto 0);
-			entr5	:	in unsigned(15 downto 0);
-			entr6	:	in unsigned(15 downto 0);
-			entr7	:	in unsigned(15 downto 0);
-			op		: 	in unsigned(2 downto 0);
+			op		: 	in std_logic;
 			saida	:	out unsigned(15 downto 0)
 			);
 	end component;
-	signal entr0,entr1,entr2,entr3,entr4,entr5,entr6,entr7,saida:	unsigned(15 downto 0);
-	signal op:	unsigned(2 downto 0);
+
+	signal entr0,entr1,saida:	unsigned(15 downto 0);
+	signal op:	std_logic;
 
 	begin
 		utt: mux16bits port map(
 			entr0=>entr0,
 			entr1=>entr1,
-			entr2=>entr2,
-			entr3=>entr3,
-			entr4=>entr4,
-			entr5=>entr5,
-			entr6=>entr6,
-			entr7=>entr7,
 			op=>op,
 			saida=>saida
 			);
 
 	process
 		begin
-			op<="000";
+			op<='0';
+			wait for 90 ns;
+			entr0<="0000000000000001";
+			op<='1';
+			wait for 90 ns;
+			entr1<="0000000000000011";
+			op<='0';
+			wait for 90 ns;
+			entr0<="0000000000000001";
+			op<='1';
 			wait for 90 ns;
 			entr1<="0000000000000001";
-			op<="001";
+			op<='0';
 			wait for 90 ns;
-			entr2<="0000000000000011";
-			op<="010";
+			entr0<="0000000000000001";
+			op<='1';
 			wait for 90 ns;
-			entr3<="0000000000000001";
-			op<="011";
+			entr1<="0000000000000001";
+			op<='0';
 			wait for 90 ns;
-			entr4<="0000000000000001";
-			op<="100";
-			wait for 90 ns;
-			entr5<="0000000000000001";
-			op<="101";
-			wait for 90 ns;
-			entr6<="0000000000000001";
-			op<="110";
-			wait for 90 ns;
-			entr7<="0000000000000001";
-			op<="111";
+			entr1<="0000000000000001";
+			op<='1';
 			wait for 90 ns;
 
 		wait;
